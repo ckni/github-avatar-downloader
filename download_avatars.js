d56c6bcdcfed6248fc383200dc8f38f83a26495b
Chester Ni <kaichesterni@gmail.com>
2017-07-03 17:22:04 -0700

    Updated .gitignore and main JS

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,11 @@
 /*jshint esversion: 6 */
-
 const request = require("request");
 const fs = require("fs");
+console.log("Welcome to GitHub Avatar Downloader v1.0.0");
 
-console.log("Welcome to the GitHub Avatar Downloader!");
-
-// Gets and saves the avatars of all contributors to a repo
-
-function getAvatarLinks(repoOwner, repoName) {
+// Gets all contributors of a repo
+function getContributors(repoOwner, repoName, callback) {
+  // Define options for requesting contributor list of a repo
   const options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -15,34 +13,39 @@ function getAvatarLinks(repoOwner, repoName) {
     }
   };
 
+  // Request contributor list of a repo
   request(options, (error, response, body) => {
     if (error) {
-      return console.log("Error retrieving contributor list.");
+      return console.log("Error: failed to request contributor list");
     } else {
-      const body = JSON.parse(response.body);
-
-      if (!fs.existsSync("./avatars")) {
-        fs.mkdir("./avatars", (error) => {
-          if (error) {
-            return console.log("Error:", error);
-          }
-        });
-      }
-
-      body.forEach((user, i) => {
-        request(user.avatar_url).pipe(fs.createWriteStream(`./avatars/${i}.jpg`));
-      });
+      console.log("Response Status Code:", response.statusCode);
+      callback(JSON.parse(body));
     }
   });
 }
 
+// Downloads avatars of all contributors
+function downloadAvatars(body) {
+  // Create ./avatars directory if it does not already exists
+  if (!fs.existsSync("./avatars")) {
+    fs.mkdir("./avatars", (error) => {
+      if (error) {
+        return console.log("Error: failed to create avatar directory");
+      }
+    });
+  }
+
+  // Save the avatar of each user into the ./avatars directory
+  for (var user in body) {
+    request(body[user].avatar_url).pipe(fs.createWriteStream(`./avatars/${body[user].login}.jpg`));
+  }
+}
+
 // Takes in repoOwner and repoName then runs functions
-
-const repoOwner = "jquery";
-const repoName = "jquery";
-
+const repoOwner = process.argv[2];
+const repoName = process.argv[3];
 if (repoOwner && repoName) {
-  getAvatarLinks(repoOwner, repoName);
+  getContributors(repoOwner, repoName, downloadAvatars);
 } else {
-  console.log("Error: missing arguments. Syntax: node download_avatars.js <Repo Owner> <Repo Name>");
+  console.log("Missing arguments. Syntax: node download_avatars.js <Repo Owner> <Repo Name>");
 }
